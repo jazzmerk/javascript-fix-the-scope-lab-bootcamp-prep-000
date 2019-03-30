aded7be6fe1d407728f9e0e0432ac96696d76e85
James Merkin <jnmerk@gmail.com>
2019-03-30 16:45:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-function test(){
+
 var theFunk;
 theFunk=funkyFunction();
-}
+
 
 
 //https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/functions-and-scope/fix-the-scope-lab#
